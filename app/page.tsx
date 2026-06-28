de7c6cab59c1fecc7a151f2419e735d4fdb8f0f1
Derovanti Richardson <nxxfriday@gmail.com>
2026-06-27 20:25:15 -0400

    Add platforms section to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,11 @@ import Navbar from "@/components/Navbar";
 import Hero from "@/components/home/Hero";
 import Services from "@/components/home/Services";
 import Industries from "@/components/home/Industries";
+import Partners from "@/components/home/Partners";
+import Platforms from "@/components/home/Platforms";
 import Process from "@/components/home/Process";
 import CTA from "@/components/home/CTA";
 import Footer from "@/components/home/Footer";
-import Platforms from "@/components/home/Platforms";
-import Partners from "@/components/home/Partners";
 
 export default function Home() {
   return (
@@ -15,6 +15,8 @@ export default function Home() {
       <Hero />
       <Services />
       <Industries />
+      <Partners />
+      <Platforms />
       <Process />
       <CTA />
       <Footer />
